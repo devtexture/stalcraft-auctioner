54d518c4f9a5c788986c6c8e4026df0acd335a6d
milk <milkshizodev@gmail.com>
2025-05-10 22:15:47 +0300

    gaddem

---
+++ client.py
@@ -0,0 +1,25 @@
+import dotenv, telebot, os, importlib;
+from glob import glob;
+
+from telebot import types;
+env = dotenv.dotenv_values('.env');
+
+client = telebot.TeleBot( env['TG_TOKEN'] );
+
+def load_handlers( client );
+    handler_files =  glob('handlers/*.py', recursive=True);
+
+    for file_path in handler_files:
+        module_path = file_path.replace('/', '.')[:-3];
+
+        try:
+            module = importlib.import_module(module_path);
+            if hasattr(module, 'register_handler'):
+                module.register_handler(client);
+                print(f'сколько? пять гривен? а дешевле есть? {module_path}');
+        except Exception as e:
+            print(f'окак {module_path} -> {str(e)}')
+
+if __name__ == '__main__':
+    load_handlers();
+    client.infinity_polling();
---
+++ handlers/objects/items.py
@@ -0,0 +1,77 @@
+import dotenv, github;
+import base64, json;
+
+from dataclasses import dataclass;
+from typing import Dict, List, Optional
+
+import concurrent.futures;
+
+token = dotenv.get_key('.env', 'GH_TOKEN')
+db_repo = 'EXBO-Studio/stalcraft-database';
+
+def get_content(path):
+    g = github.Github(token);
+    repo = g.get_repo(db_repo);
+
+    try:
+        file = repo.get_contents(f'ru/{path}');
+
+        if path.lower().endswith('.json'):
+            return json.loads( base64.b64decode(file.content).decode('utf-8') )
+        return file.download_url;
+    except Exception as e:
+        print(f'ебанное очко {path}: {str(e)}')
+        return None;
+
+@dataclass
+class Item:
+    item_data: str
+    item_icon: str
+    item_id: str
+    name_ru: str
+    name_en: str
+    
+    @classmethod
+    def from_dict(cls, data: Dict) -> 'Item':
+
+        parts = data['data'].split('/');
+        id = parts[-1].split('.')[0];
+
+        return cls(
+            item_data=data['data'][1:],
+            item_icon=data['icon'][1:],
+            item_id=id,
+            name_ru=data['name']['lines']['ru'],
+            name_en=data['name']['lines']['en']
+        )
+    
+    def get_image(self):
+        return get_content(self.item_icon);
+
+class ItemDatabase:
+
+    def __init__(self):
+        self._items: Dict[str, Item] = {}
+
+    def load_items(self, path: str = "listing.json") -> None:
+        json = get_content(path);
+
+        if isinstance(json, list):
+            for item in json:
+                _item = Item.from_dict(item)
+                self._items[ _item.item_id ] = _item
+        else:
+            raise ValueError("Неверный формат данных. Ожидается список предметов.")
+        
+    def get_item(self, id: str) -> Optional[Item]:
+        return self._items.get(id)
+    
+    def search_items(self, term: str, lang: str = None) -> List[Item]:
+        results = [];
+        term = term.lower();
+
+        for item in self._items.values():
+            if (lang == 'ru' and term in item.name_ru.lower()) or (lang == 'en' and term in item.name_en.lower()) or (term in item.name_ru.lower() or term in item.name_en.lower()):
+                results.append(item);
+
+        return results;
